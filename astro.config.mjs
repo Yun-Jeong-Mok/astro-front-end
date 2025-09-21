7194e6b36331fa6881258ec0493d9d21a5c316bb
dbswdjahr <yjm011019@naver.com>
2025-09-22 00:44:49 +0900

    tls key added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    server: {
+      https: {
+        cert: 'localhost+1.pem', 
+        key: 'localhost+1-key.pem',
+      },
+      host: true,
+      allowedHosts: true,
+    },
+  },
 });
